4dd91f9fab2fbbd5270d74aedaced57d2fd1960f
Jens Pfahl <jens.pfahl@mailbox.org>
2024-09-04 21:32:06 +0200

    start page

---
+++ lib/ui/start_page.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+
+import 'game_ground.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 24, end: 32).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    _controller.repeat(reverse: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorObservers: [FlutterSmartDialog.observer],
+      builder: FlutterSmartDialog.init(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+        useMaterial3: true,
+      ),
+      home: Builder(builder: (context) {
+        return _buildStartPage(context);
+      }),
+    );
+  }
+
+  Widget _buildStartPage(BuildContext context) {
+    return Container(
+      color: Theme.of(context).colorScheme.surface,
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Padding(
+          padding: const EdgeInsets.all(64.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildGameLogo(),
+              GestureDetector(
+                onTap: () async {
+                  if (context.mounted) {
+                    SmartDialog.showLoading(msg: "Loading game ...");
+                    await Future.delayed(const Duration(seconds: 1));
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const Hyle9Ground();
+                    }));
+                  }
+                },
+                child: _buildChip("Start", Colors.red, 60, 16, 10),
+              ),
+              GestureDetector(
+                onTap: () {
+                  SmartDialog.showNotify(
+                    msg: 'Not yet implemented!',
+                    clickMaskDismiss: true,
+                    displayTime: const Duration(seconds: 3),
+                    notifyType: NotifyType.error,
+                  );
+                },
+                child: _buildChip("Resume", Colors.lightBlueAccent, 60, 16, 10),
+              ),
+              GestureDetector(
+                onTap: () {
+                  SmartDialog.show(builder: (_) {
+                    return Container(
+                      height: 180,
+                      width: 180,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Leave the game?',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            OutlinedButton(
+                                style: OutlinedButton.styleFrom(
+                                    foregroundColor: Colors.lightGreenAccent),
+                                onPressed: () {
+                                  SystemNavigator.pop();
+                                },
+                                child: Text("YES")),
+                            OutlinedButton(
+                                style: OutlinedButton.styleFrom(
+                                    foregroundColor: Colors.lightGreenAccent),
+                                onPressed: () {
+                                  SmartDialog.dismiss();
+                                },
+                                child: Text("NO"))
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+                },
+                child: _buildChip("Exit", Colors.brown, 40, 16, 10),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildChip(String text, Color color, double radius, double textSize,
+      double padding) {
+    return Padding(
+      padding: EdgeInsets.all(padding),
+      child: CircleAvatar(
+        backgroundColor: color,
+        radius: radius,
+        child: Text(text,
+            style: TextStyle(
+              fontSize: textSize,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            )),
+      ),
+    );
+  }
+
+  Widget _buildGameLogo() {
+    const chipPadding = 1.0;
+    return Column(children: [
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildChip("H", Colors.redAccent, 30, 20, chipPadding),
+          _buildChip("Y", Colors.blueGrey, 30, 20, chipPadding),
+          Text("9",
+              style: TextStyle(
+                  fontSize: _animation.value,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold))
+        ],
+      ),
+      Row(
+        children: [
+          _buildChip("L", Colors.orangeAccent, 30, 20, chipPadding),
+          _buildChip("E", Colors.cyanAccent, 30, 20, chipPadding),
+        ],
+      ),
+    ]);
+  }
+}
